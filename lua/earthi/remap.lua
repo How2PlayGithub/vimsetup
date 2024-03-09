2d6104b18de15d7bb517540072c6534661f87b0b
How2PlayGithub <how2bypass@gmail.com>
2024-03-10 08:16:26 +1300

    keybinds updated

--- lua/earthi/remap.lua
+++ lua/earthi/remap.lua
@@ -2,3 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+local builtin = require 'telescope.builtin'
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, {desc = '[S]earch [H]elp'})
