67c6d865217f0c8bb15b1e7d4a2f7f4fb105ceed
How2PlayGithub <134759103+How2PlayGithub@users.noreply.github.com>
2024-04-16 22:37:38 +1200

    updated

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,12 +11,12 @@ lsp_zero.on_attach(function(client, bufnr)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer'},
+  ensure_installed = {'rust_analyzer'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
--- lua/earthi/remap.lua
+++ lua/earthi/remap.lua
@@ -5,7 +5,4 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
-local builtin = require 'telescope.builtin'
-vim.keymap.set("n", "<leader>sh", builtin.help_tags, {desc = '[S]earch [H]elp'})
-
 vim.api.nvim_set_keymap('n', '<leader>c', ':lua switch_windows()<CR>', { noremap = true })
