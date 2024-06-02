30a254252f636bf280e440cf1ab84aa23589e97f
How2PlayGithub <134759103+How2PlayGithub@users.noreply.github.com>
2024-06-03 10:12:51 +1200

    Delete lua/earthi directory

--- lua/earthi/remap.lua
+++
@@ -1,8 +0,0 @@
-require('earthi.functions')
-
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
-vim.api.nvim_set_keymap('n', '<leader>c', ':lua switch_windows()<CR>', { noremap = true })
